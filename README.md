1fe600053dbcd634b5e10ecff54bc0d22f59d250
vi-vi-3482 <78184297+ViViViViViViViViViViViVIVi@users.noreply.github.com>
2025-10-08 17:19:33 -0700

    Created camera class to control the camera functionality in a modular fashion and stream the video output

---
+++ README.md
@@ -0,0 +1,10 @@
+# Realsense Mediapipe Tracking
+
+This is a project to integrate the use of a Realsense d435 depth camera with Mediapipe for tracking the position of a subjects hand. 
+
+Using UV for package management. 
+
+Required external libraries:
+https://github.com/IntelRealSense/librealsense
+
+Licensed under 
---
+++ src/realsense_mediapipe_tracking/camera.py
@@ -0,0 +1,65 @@
+import pyrealsense2 as rs
+import cv2
+import numpy as np
+
+class realsenseCamera:
+    def __init__(self, width=640, height=480, fps=30):
+
+        try:
+            self.pipe = rs.pipeline()
+            self.config = rs.config()
+
+            self.config.enable_stream(rs.stream.depth, width, height, rs.format.z16, fps)
+            self.config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, fps)
+
+            self.profile = self.pipe.start(self.config)
+
+
+            self.align = rs.align(rs.stream.color)
+
+            print("Camera connected")
+        except Exception as e:
+            print("Likely no camera connected. \n", e)
+    
+    def get_frames(self):
+
+        frames = self.pipe.wait_for_frames()
+        frames = self.align.process(frames)
+        color_frame = frames.get_color_frame()
+        depth_frame = frames.get_depth_frame()
+
+        if not color_frame or not depth_frame:
+            return None, None
+        
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        return color_image, depth_image
+
+    def stream(self, display_rgb=False, display_depth=False):
+            """Continuously stream and optionally display color+depth."""
+            try:
+                while True:
+                    color_image, depth_image = self.get_frames()
+                    if color_image is None or depth_image is None:
+                        continue
+
+                    if display_rgb:
+                        cv2.imshow('Color', color_image)
+                    if display_depth:
+                        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+                        cv2.imshow('Depth', depth_colormap)
+
+                    if cv2.waitKey(1) & 0xFF == 27:  # ESC to exit
+                        break
+            finally:
+                self.stop()
+    def stop(self):
+        """Stop the streaming process."""
+        self.pipe.stop()
+        cv2.destroyAllWindows()
+        print("camera stopped")
+
+if __name__ == "__main__":
+    cam = realsenseCamera()
+    cam.stream(display_rgb=True, display_depth=True)
